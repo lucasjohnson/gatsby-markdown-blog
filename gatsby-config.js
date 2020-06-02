b4254af9ebfd29c8ebaa471f8071a5988770cfce
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-02 18:21:03 -0400

    basic graphql query for homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	siteMetadata: {
-		title: ``,
+		siteName: `My Gatsby Starter`,
 		description: ``,
 		author: ``
 	},
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,33 @@
-import React, { FunctionComponent } from 'react';
+import React, { Component } from 'react';
+import { graphql, PageProps } from 'gatsby';
 import Layout from '../components/layout';
 
-const Index: FunctionComponent = () => (
-	<Layout>
-		<h1>My Gatsby Starter</h1>
-	</Layout>
-);
+interface IndexProps extends PageProps {
+	data: {
+		site: {
+			siteMetadata: {
+				siteName: string;
+			};
+		};
+	};
+}
 
-export default Index;
+export const pageQuery = graphql`
+	query IndexQuery {
+		site {
+			siteMetadata {
+				siteName
+			}
+		}
+	}
+`;
+
+export default function IndexPage(props: IndexProps) {
+	const { siteName } = props.data.site.siteMetadata;
+
+	return (
+		<Layout>
+			<h1>{siteName}</h1>
+		</Layout>
+	);
+}
