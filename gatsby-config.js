b6998a7b509d12521c77a036171a5ead253437e2
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-03 13:19:52 -0400

    added and configured manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,20 @@ module.exports = {
 		description: ``,
 		author: ``
 	},
-	plugins: [`gatsby-plugin-sass`]
+	plugins: [
+		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `My Gatsby Starter`,
+				short_name: `My Gatsby Starter`,
+				start_url: `/`,
+				background_color: `#ffffff`,
+				theme_color: `#7f1980`,
+				display: `standalone`,
+				icon: `src/images/icon.png`,
+				crossOrigin: `use-credentials`
+			}
+		}
+	]
 };
