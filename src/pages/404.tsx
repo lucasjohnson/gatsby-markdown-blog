d2525b857a2149c8e74f3f2ce7cac0e7101560db
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-04 18:29:15 -0400

    component name typo

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 const ErrorPage: FunctionComponent = () => (
 	<Layout>
