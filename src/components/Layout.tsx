e6be6f27fca704de3b247ddbf14faab423c520c4
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-05 18:26:01 -0400

    added tab focusing to buttons and created skip to main content button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,9 @@ import Footer from './Footer';
 
 const Layout: FunctionComponent = ({ children }) => (
 	<React.Fragment>
+		<a className="skip-button" href="#mainContent">
+			Skip to main content
+		</a>
 		<Header />
 		<main id="mainContent">{children}</main>
 		<Footer />
