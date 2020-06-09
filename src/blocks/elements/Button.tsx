bf4fe99d40a4695357fef0729736b131db535670
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-09 13:36:27 -0400

    removed unneeded fragment

--- src/blocks/elements/Button.tsx
+++ src/blocks/elements/Button.tsx
@@ -2,16 +2,14 @@ import React, { FunctionComponent } from 'react';
 
 interface ButtonProps {
 	variant: 'primary' | 'secondary';
-	onClick(event: React.MouseEvent<HTMLButtonElement>): React.ReactNode;
+	onClick?: () => void;
 }
 
 const Button: FunctionComponent<ButtonProps> = ({ variant, onClick, children }) => {
 	return (
-		<React.Fragment>
-			<button className={`button ${variant}`} onClick={onClick}>
-				{children}
-			</button>
-		</React.Fragment>
+		<button className={`button ${variant}`} onClick={onClick}>
+			{children}
+		</button>
 	);
 };
 
