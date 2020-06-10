1e6464c0276d44eb9764bb973c44cf5ed30587c8
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-10 18:10:39 -0400

    updated data types and constants of grid card component

--- src/blocks/elements/GridCard.tsx
+++ src/blocks/elements/GridCard.tsx
@@ -2,15 +2,17 @@ import React, { FunctionComponent } from 'react';
 import { Link } from 'gatsby';
 
 interface GridCardProps {
-	data: { abstract: string; banner: string; data: string; title: string };
+	data: { abstract: string; banner: string; slug: string; title: string };
 }
 
 const GridCard: FunctionComponent<GridCardProps> = ({ data }) => {
+	const { abstract, banner, slug, title } = data;
+
 	return (
-		<Link className="grid-card" to="/" title={data.title}>
+		<Link className="grid-card" to={slug} title={title}>
 			<article className="card-content">
-				<h4 className="heading-4">{data.title}</h4>
-				<p className="body-copy">{data.abstract}</p>
+				<h4 className="heading-4">{title}</h4>
+				<p className="body-copy">{abstract}</p>
 				<span className="tag">Test</span>
 			</article>
 		</Link>
