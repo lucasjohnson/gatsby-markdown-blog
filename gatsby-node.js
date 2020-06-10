6c58769caf09bfc7d803a5b79c4b980c6781ac00
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-10 18:09:07 -0400

    created slug field to query and logic to generate blog post urls

---
+++ gatsby-node.js
@@ -0,0 +1,50 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+	const { data } = await graphql(`
+		query {
+			allMarkdownRemark {
+				edges {
+					node {
+						fields {
+							slug
+						}
+						frontmatter {
+							title
+						}
+					}
+				}
+			}
+		}
+	`);
+
+	const posts = data.allMarkdownRemark.edges;
+
+	posts.forEach(({ node }, index) => {
+		const slug = node.fields.slug;
+		actions.createPage({
+			path: slug,
+			component: require.resolve(`./src/templates/post.tsx`),
+			context: {
+				prev: index === 0 ? null : posts[index - 1].node,
+				next: index === posts.length - 1 ? null : posts[index + 1].node
+			}
+		});
+	});
+
+	if (data.errors) {
+		reporter.panicOnBuild(`Error while running GraphQL query.`);
+		return;
+	}
+};
+
+const { createFilePath } = require(`gatsby-source-filesystem`);
+exports.onCreateNode = ({ node, actions, getNode }) => {
+	const { createNodeField } = actions;
+	if (node.internal.type === `MarkdownRemark`) {
+		const value = createFilePath({ node, getNode });
+		createNodeField({
+			name: `slug`,
+			node,
+			value
+		});
+	}
+};
