e7c73f89f92cf13f171cce7873b5abfa2c0f0892
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-10 14:02:36 -0400

    grid component renamed

--- src/blocks/elements/GridBox.tsx
+++ src/blocks/elements/GridBox.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 
-interface GridProps {
+interface GridBoxProps {
 	column: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 	columnSm?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 	columnMd?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
@@ -10,7 +10,7 @@ interface GridProps {
 	variant: 'grid-box';
 }
 
-const Grid: FunctionComponent<GridProps> = ({
+const GridBox: FunctionComponent<GridBoxProps> = ({
 	column,
 	columnSm,
 	columnMd,
@@ -37,4 +37,4 @@ const Grid: FunctionComponent<GridProps> = ({
 	);
 };
 
-export default Grid;
+export default GridBox;
--- src/pages/layouts.tsx
+++ src/pages/layouts.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
-import Grid from '../blocks/elements/Grid';
+import GridBox from '../blocks/elements/Grid';
 import GridCard from '../blocks/elements/GridCard';
 
 const data = [
@@ -46,17 +46,17 @@ const LayoutsPage: FunctionComponent = () => (
 	<Layout>
 		<SEO title="Layouts" />
 		<section className="block">
-			<Grid variant="grid-flex">
+			<div className="grid-flex">
 				<div className="column-12">
 					<h1 className="heading-1">Layouts</h1>
 					<h2 className="heading-2">Grid Cards</h2>
-					<Grid variant="grid">
+					<GridBox variant="grid-box" column={1} columnMd={2} columnLg={4}>
 						{data.map((item, index) => {
 							return <GridCard data={item} key={index} />;
 						})}
 					</Grid>
 				</div>
-			</Grid>
+			</div>
 		</section>
 	</Layout>
 );
