4f59cb1d8ed6c74013b92f1ea62ae796654d89d7
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-12 17:42:11 -0400

    logic to query images from frontmatter

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,8 +36,11 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 	}
 };
 
+const { fmImagesToRelative } = require(`gatsby-remark-relative-images`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
+	fmImagesToRelative(node);
 	const { createNodeField } = actions;
 	if (node.internal.type === `MarkdownRemark`) {
 		const value = createFilePath({ node, getNode });
