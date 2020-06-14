3e69a80f5aa2b0438036a0b3bd23feb41b0e1042
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-14 19:49:31 -0400

    updated service worker config

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,13 +71,6 @@ module.exports = {
 				cache_busting_mode: `none`
 			}
 		},
-		{
-			resolve: `gatsby-plugin-offline`,
-			options: {
-				workboxConfig: {
-					globPatterns: [`**/*`]
-				}
-			}
-		}
+		`gatsby-plugin-offline`
 	]
 };
