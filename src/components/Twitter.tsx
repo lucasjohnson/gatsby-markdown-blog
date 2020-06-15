60e467f857326d4c9dd8c7560b88925963ef80c0
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-15 17:25:22 -0400

    added twitter handle query

--- src/components/Twitter.tsx
+++ src/components/Twitter.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import socialData from '../../content/site/social.yml';
 
+console.log(socialData);
+
 type TwitterProps = {
 	title: string;
 	description: string;
@@ -18,8 +20,8 @@ const Twitter = ({ title, description, url, banner, bannerAlt }: TwitterProps) =
 		<meta name="twitter:url" content={url} />
 		<meta name="twitter:image" content={banner} />
 		<meta name="twitter:image:alt" content={bannerAlt} />
-		<meta name="twitter:site" content={`@_lucasjohnson`} />
-		<meta name="twitter:creator" content={`@_lucasjohnson`} />
+		<meta name="twitter:site" content={socialData.twitterHandle} />
+		<meta name="twitter:creator" content={socialData.twitterHandle} />
 	</Helmet>
 );
 
