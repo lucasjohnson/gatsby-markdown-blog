1092c879048bf490ba279d64eae0cabd5b199bcd
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-16 16:31:37 -0400

    added basic navigation to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'gatsby';
+import Button from '../templates/elements/Button';
 import siteData from '../../content/site/data.yml';
 
 const Header = (): ReactElement => {
+	const handleClick = (): void => {};
+
 	const { siteTitle } = siteData;
 
 	return (
@@ -9,7 +13,23 @@ const Header = (): ReactElement => {
 			<div className="block">
 				<div className="grid-flex">
 					<div className="column-12">
-						<h4 className="heading-4">{siteTitle}</h4>
+						<h4 className="heading-4">
+							<Link className="site-title" to="/" title="Hompage link">
+								{siteTitle}
+							</Link>
+						</h4>
+						<nav className="main-navigation">
+							<ul className="navigation-items">
+								<li className="navigation-item">
+									<Link className="navigation-link" to="/blog" title="Blog page link">
+										Blog
+									</Link>
+								</li>
+							</ul>
+						</nav>
+						<Button variant={`secondary`} onClick={handleClick}>
+							Contact
+						</Button>
 					</div>
 				</div>
 			</div>
