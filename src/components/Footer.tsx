1334199dfead0647f6b79c70718b3837ee4efe8d
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-18 16:50:10 -0400

    basic breadcrumb logic created in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,40 @@
 import React, { FunctionComponent } from 'react';
+import { Link } from 'gatsby';
 
-const Footer: FunctionComponent = () => (
-	<footer>
-		<div className="block">
-			<div className="grid-flex">
-				<div className="column-12"></div>
+const Footer: FunctionComponent = () => {
+	const pathname: string = typeof window !== `undefined` ? window.location.pathname : ``;
+	const pathCrumbs: string[] = pathname.split(`/`);
+
+	pathname.length === 1 && pathCrumbs.pop();
+
+	const renderBreadcrumbs = () =>
+		pathCrumbs.map((crumb: string, index: number) => {
+			return (
+				<li className="breadcrumb-item" key={index}>
+					<Link
+						className="breadcrumb-link"
+						to={`/${crumb && crumb}`}
+						title={`Link to ${crumb.length === 0 ? `Home` : crumb} page`}
+					>
+						{crumb.length === 0 ? `Home` : crumb}
+					</Link>
+				</li>
+			);
+		});
+
+	return (
+		<footer>
+			<div className="block">
+				<div className="grid-flex">
+					<div className="column-12">
+						<nav className="breadcrumb-navigation">
+							<ul className="breadcrumb-items">{renderBreadcrumbs()}</ul>
+						</nav>
+					</div>
+				</div>
 			</div>
-		</div>
-	</footer>
-);
+		</footer>
+	);
+};
 
 export default Footer;
