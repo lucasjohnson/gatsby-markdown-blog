17d251aea1bc4e789fb0032182534a3d7d7e20ff
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-18 12:00:10 -0400

    Merge branch 'master' of github.com:lucasjohnson/gatsby-netlify-cms-starter

--- src/templates/author.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from 'react';
-import Layout from '../components/Layout';
-const Author = (): ReactElement => {
-	return <Layout></Layout>;
-};
-
-export default Author;
