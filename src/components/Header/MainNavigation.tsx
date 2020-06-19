d2f6f99597caddf6ce94108a0d7b786e700d2d40
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-19 13:54:57 -0400

    sanitiazed slugs

--- src/components/Header/MainNavigation.tsx
+++ src/components/Header/MainNavigation.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import mainMenuData from '../../../content/site/main-navigation.yml';
+import sanitizeSlug from '../../helpers/utils';
 
 const MainNavigation: React.FC = () => {
 	const renderDropdownItems: Function = (dropdownItems: [], dropdownTitle: string) =>
 		dropdownItems.map((item: { page: string }, index: number) => {
-			let { page: title } = item;
+			const { page: title } = item;
 
 			return (
 				<li className="dropdown-item" key={index}>
 					<Link
 						className="dropdown-link"
-						to={`/${dropdownTitle.replace(/ /g, `-`).toLowerCase()}/${(title = title
-							.replace(/ /g, `-`)
-							.toLowerCase())}`}
+						to={`/${sanitizeSlug(dropdownTitle)}/${sanitizeSlug(title)}`}
 						title={`Link to ${title} page`}
 					>
 						{title}
@@ -30,11 +29,7 @@ const MainNavigation: React.FC = () => {
 
 			return (
 				<li className="navigation-item" key={index}>
-					<Link
-						className="navigation-link"
-						to={`/${title.replace(/ /g, `-`).toLowerCase()}`}
-						title={`Link to ${title} page`}
-					>
+					<Link className="navigation-link" to={`/${sanitizeSlug(title)}`} title={`Link to ${title} page`}>
 						{title}
 					</Link>
 					{item.dropdownItems && <ul className="dropdown-items">{renderDropdownItems(item.dropdownItems, title)}</ul>}
