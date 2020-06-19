affe212db0448e688d50f54c38735af73d355098
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-19 12:16:14 -0400

    logic to output main navigation

--- src/components/Header/MainNavigation.tsx
+++ src/components/Header/MainNavigation.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import mainMenu from '../../../content/site/main-navigation.yml';
+import mainMenuData from '../../../content/site/mainMenuItems.yml';
 
 const MainNavigation: React.FC = () => {
-	const renderMenuItems: Function = () => {
-		return (
-			<li className="navigation-item">
-				<Link className="navigation-link" to="/blog" title="Blog page link">
-					Blog
-				</Link>
-			</li>
-		);
-	};
+	const renderDropdownItems: Function = (dropdownItems: []) =>
+		dropdownItems.map((item: { page: string }, index: number) => {
+			const { page: title } = item;
+
+			return (
+				<li className="dropdown-item" key={index}>
+					<Link className="dropdown-link" to={`/`} title={`Link to ${title} page`}>
+						{title}
+					</Link>
+				</li>
+			);
+		});
+
+	const { mainMenuItems } = mainMenuData;
+
+	const renderMainMenuItems: Function = () =>
+		mainMenuItems.map((item: { page: string; dropdownItems: {}[] }, index: number) => {
+			const { page: title } = item;
+
+			return (
+				<li className="navigation-item" key={index}>
+					<Link className="navigation-link" to={`/`} title={`Link to ${title} page`}>
+						{title}
+					</Link>
+					{item.dropdownItems && <ul className="dropdown-items">{renderDropdownItems(item.dropdownItems)}</ul>}
+				</li>
+			);
+		});
+
 	return (
 		<nav className="main-navigation">
-			<ul className="navigation-items">{renderMenuItems()}</ul>
+			<ul className="navigation-items">{renderMainMenuItems()}</ul>
 		</nav>
 	);
 };
