a8c55e1082f498b7e0baa8dd324511d1e91ef24c
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-19 13:34:54 -0400

    updated services component with return/types and title conditional

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,18 +1,25 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/Head/SEO';
 
-const Service = ({ pageContext }): ReactElement => {
+interface Props {
+	pageContext: {
+		title: string;
+	};
+}
+
+const Service: React.FC<Props> = ({ pageContext }) => {
 	const { title } = pageContext;
+	const pageTitle = title ? title : `Services`;
 
 	return (
 		<Layout>
-			<SEO title={title} />
+			<SEO title={pageTitle} />
 			<section className="Topic">
 				<div className="block">
 					<div className="grid-flex">
 						<div className="column-12">
-							<h1 className="heading-1">{title}</h1>
+							<h1 className="heading-1">{pageTitle}</h1>
 						</div>
 					</div>
 				</div>
