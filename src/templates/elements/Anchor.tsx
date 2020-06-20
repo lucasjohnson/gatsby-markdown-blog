17667657e75f50dca948543db264064837a6c71c
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-20 12:57:14 -0400

    button link and link component combined

--- src/templates/elements/Anchor.tsx
+++ src/templates/elements/Anchor.tsx
@@ -1,33 +1,52 @@
 import React from 'react';
-import { Link as GatsbyLink } from 'gatsby';
+import { Link } from 'gatsby';
 
 type AnchorProps = {
 	className?: string;
 	title: string;
 	url: string;
-	variant: 'external' | 'internal';
+	variant:
+		| 'link'
+		| 'button primary'
+		| 'button secondary'
+		| 'link external'
+		| 'button external primary'
+		| 'button external secondary';
 };
 
 const Anchor: React.FC<AnchorProps> = ({ className, title, url, variant, children }) => {
-	if (variant === `internal`) {
-		return (
-			<GatsbyLink className={`link ${className}`} to={url} title={title}>
-				{children}
-			</GatsbyLink>
-		);
+	let anchor;
+
+	switch (variant) {
+		case `link`:
+		case `button primary`:
+		case `button secondary`:
+			anchor = (
+				<Link className={`${variant} ${className}`} to={url} title={title}>
+					{children}
+				</Link>
+			);
+			break;
+		case `link external`:
+		case `button external primary`:
+		case `button external secondary`:
+			anchor = (
+				<a
+					className={`${variant} ${className}`}
+					href={url}
+					title={`${title} opens in new tab`}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{children}
+				</a>
+			);
+			break;
+		default:
+			return null;
 	}
 
-	return (
-		<a
-			className={`link ${className}`}
-			href={url}
-			title={`${title} opens in new tab`}
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			{children}
-		</a>
-	);
+	return anchor;
 };
 
 export default Anchor;
