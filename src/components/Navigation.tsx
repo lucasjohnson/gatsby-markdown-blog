54007c5fa2ec00ce2a1223f6d901ec30a600b781
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-21 14:45:44 -0400

    sub navigation added to navigation element

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,35 +3,31 @@ import { Link } from 'gatsby';
 import slugify from '../helpers/utils';
 
 interface NavigationProps {
-	data: {
-		item: string;
-		index: number;
-		subItems?: {
-			item: string;
-			index: number;
-		}[];
-	}[];
+	data: {}[];
 	title: string;
 	variant: 'block' | 'inline';
 }
 
 const Navigation: React.FC<NavigationProps> = ({ data, title, variant }) => {
-	const renderItems: Function = () =>
-		data.map((item, index) => {
+	const renderItems: Function = (menuItems: { item: string; index: number }[]) =>
+		menuItems.map((item: { item: string; subItems?: { item: string } }, index: number) => {
 			const { item: title } = item;
 
 			return (
-				<li className="item" key={index}>
-					<Link className="link" to={`/${slugify(title)}`} title={title}>
-						{title}
-					</Link>
-				</li>
+				<React.Fragment>
+					<li className="item" key={index}>
+						<Link className="link" to={`/${slugify(title)}`} title={title}>
+							{title}
+						</Link>
+					</li>
+					{item.subItems !== undefined && renderItems(item.subItems)}
+				</React.Fragment>
 			);
 		});
 
 	return (
 		<nav className={`navigation ${variant} ${title}`}>
-			<ul className="navigation-items">{renderItems()}</ul>
+			<ul className="navigation-items">{renderItems(data)}</ul>
 		</nav>
 	);
 };
