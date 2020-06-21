f2fb9eee1fcd2f91438f807951fa128ea00730aa
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-21 15:34:57 -0400

    footer navigation component updated to use navigation component

--- src/components/Footer/FooterNavigation.tsx
+++ src/components/Footer/FooterNavigation.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import GridBox from '../GridBox';
+import Navigation from '../Navigation';
+import data from '../../../content/navigation/footer-navigation.yml';
 
 const FooterNavigation: React.FC = () => {
-	return <p>Footer</p>;
+	const { menuColumns } = data;
+
+	const renderMenuColumns: Function = () =>
+		menuColumns.map((item: { title: string; menuItems: {}[] }, index: number) => {
+			const { title, menuItems } = item;
+			return (
+				<div className="footer-column" key={index}>
+					<h3 className="heading-4">{title}</h3>
+					<Navigation data={menuItems} title="footer" variant="block" />
+				</div>
+			);
+		});
+
+	return (
+		<GridBox variant="grid-box" column={1} columnMd={2} columnLg={3}>
+			{renderMenuColumns()}
+		</GridBox>
+	);
 };
 
 export default FooterNavigation;
