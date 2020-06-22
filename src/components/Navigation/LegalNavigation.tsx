5cc780eb0e44a66eac8786ddbbb8c106e5092a3d
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-06-22 13:59:47 -0400

    grid layout added to legal footer

--- src/components/Navigation/LegalNavigation.tsx
+++ src/components/Navigation/LegalNavigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GridBox from '../GridBox';
 import Navigation from './Navigation';
 import legalData from '../../../content/navigation/legal-navigation.yml';
 import socialData from '../../../content/site/social.yml';
@@ -8,11 +9,11 @@ const LegalNavigation: React.FC = () => {
 	const { menuItems: socialItems } = socialData;
 
 	return (
-		<div className="legal-navigation">
+		<GridBox variant="grid-box" column={1} columnMd={2} columnLg={3}>
 			<p className="body-copy">{legalCopy}</p>
 			<Navigation data={legalItems} title="legal" variant="inline" />
 			<Navigation data={socialItems} title="social" variant="icon" />
-		</div>
+		</GridBox>
 	);
 };
 
