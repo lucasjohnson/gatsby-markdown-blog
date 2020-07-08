187f7d69c342900846315684e6f2946ae1e028b9
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-07-08 15:43:30 -0400

    post list component created

---
+++ src/components/PostList.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import GridBox from '../components/GridBox';
+import GridCard from '../components/GridCard';
+
+const PostList: React.FC<PostProps> = ({ posts }) => {
+	const renderCard: Function = () =>
+		posts.map((post, index) => {
+			const { node } = post;
+			const { childMarkdownRemark } = node;
+			const { frontmatter } = childMarkdownRemark;
+
+			return <GridCard data={frontmatter} key={index} />;
+		});
+
+	return (
+		<GridBox variant="grid-box" column={1} columnMd={2} columnLg={3}>
+			{renderCard()}
+		</GridBox>
+	);
+};
+
+export default PostList;
+
+interface PostProps {
+	posts: {
+		node: {
+			childMarkdownRemark: {
+				frontmatter: {
+					abstract: string;
+					path: string;
+					tags: [];
+					title: string;
+					banner: {
+						childImageSharp: {
+							fluid: {
+								aspectRatio: number;
+								base64: string;
+								sizes: string;
+								srcSet: string;
+							};
+						};
+					};
+				};
+			};
+		};
+	}[];
+}
--- src/templates/blog.tsx
+++ src/templates/blog.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import GridBox from '../components/GridBox';
-import GridCard from '../components/GridCard';
 import TagList from '../components/TagList';
+import PostList from '../components/PostList';
 import SEO from '../components/Head/SEO';
+
+const Blog: React.FC<BlogProps> = ({ pageContext }) => {
+	const { posts, tags } = pageContext;
+
+	return (
+		<Layout>
+			<SEO title="Blog" />
+			<section className="Blog block">
+				<h1 className="heading-1">Blog</h1>
+				<TagList tags={tags} variant="button" />
+				<PostList posts={posts} />
+			</section>
+		</Layout>
+	);
+};
+
+export default Blog;
 
 interface BlogProps {
 	pageContext: {
@@ -12,8 +28,8 @@ interface BlogProps {
 				childMarkdownRemark: {
 					frontmatter: {
 						abstract: string;
-						date: string;
 						path: string;
+						tags: [];
 						title: string;
 						banner: {
 							childImageSharp: {
@@ -28,34 +44,8 @@ interface BlogProps {
 					};
 				};
 			};
-		};
+		}[];
+		services: string[];
 		tags: string[];
 	};
 }
-
-const Blog: React.FC<BlogProps> = ({ pageContext }) => {
-	const { posts, tags } = pageContext;
-
-	const renderCard: Function = () =>
-		posts.map((post: { node: { childMarkdownRemark: { frontmatter: {} } } }, index: number) => {
-			const { node } = post;
-			const { childMarkdownRemark } = node;
-			const { frontmatter } = childMarkdownRemark;
-			return <GridCard data={frontmatter} key={index} />;
-		});
-
-	return (
-		<Layout>
-			<SEO title="Blog" />
-			<section className="Blog block">
-				<h1 className="heading-1">Blog</h1>
-				<TagList tags={tags} variant="button" />
-				<GridBox variant="grid-box" column={1} columnMd={2} columnLg={3}>
-					{renderCard()}
-				</GridBox>
-			</section>
-		</Layout>
-	);
-};
-
-export default Blog;
