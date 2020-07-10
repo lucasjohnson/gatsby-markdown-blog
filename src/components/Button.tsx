f0a437075653df58ae61456c7a6d1555740a8680
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-07-09 20:40:48 -0400

    base button refactored to only remove default browser css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button: React.FC<ButtonProps> = ({ children, className, onClick, variant }) => {
 	return (
-		<button className={`${className ? `${className} ` : ``}button ${variant}`} onClick={onClick}>
+		<button className={`${className ? ` ${className} ` : ``}button${variant ? ` ${variant}` : ``}`} onClick={onClick}>
 			{children}
 		</button>
 	);
@@ -13,5 +13,5 @@ export default Button;
 interface ButtonProps {
   className?: string;
   onClick?: () => void;
-	variant: 'primary' | 'secondary';
+	variant?: 'primary' | 'secondary';
 }
