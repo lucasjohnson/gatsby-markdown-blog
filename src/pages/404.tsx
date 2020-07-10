a208b1236846cbb3a2b3e2baa683d2a8460fa95d
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-07-10 15:35:07 -0400

    add services and tags links to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,9 +3,26 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/Head/SEO';
 import PostList from '../components/PostList';
+import TagList from '../components/TagList';
 
 const ErrorPage: React.FC<ErrorProps> = ({ data }) => {
   const { edges: posts } = data.allFile;
+  const allServicesArray: [] = [];
+  const allTagsArray: [] = [];
+
+	posts.forEach(({ node }) => {
+		const { services, tags } = node.childMarkdownRemark.frontmatter;
+
+    services.forEach(service => {
+      allServicesArray.includes(service) === false &&
+        allServicesArray.push(service);
+    });
+
+    tags.forEach(tag => {
+      allTagsArray.includes(tag) === false &&
+        allTagsArray.push(tag);
+    });
+	});
 
   return (
     <Layout>
@@ -13,8 +30,11 @@ const ErrorPage: React.FC<ErrorProps> = ({ data }) => {
   		<section className="Error">
         <div className="block">
           <h1 className="heading-1">404 Page Not Found</h1>
-          <h1 className="heading-2">Recent Posts</h1>
+          <p className="body-copy">The page you were looking for doesn't exist.</p>
+          <TagList copy="Services offered:" items={allServicesArray} variant="link" />
+          <h2 className="heading-2">Recent Posts</h2>
           <PostList posts={posts} />
+          <TagList copy="All tags:" items={allTagsArray} variant="link" />
         </div>
   		</section>
   	</Layout>
@@ -26,7 +46,23 @@ export default ErrorPage;
 interface ErrorProps {
 	data: {
     allFile: {
-      edges: [];
+      edges: {
+        node: {
+          childMarkdownRemark: {
+            frontmatter: {
+              banner: {
+                childImageSharp: {
+                  fluid: {};
+                };
+              };
+              path: string;
+              services: [];
+              tags: [];
+              title: string;
+            };
+          };
+        };
+      }[];
     };
 	};
 };
@@ -37,10 +73,7 @@ export const errorQuery = graphql`
       edges {
         node {
           childMarkdownRemark {
-            html
             frontmatter {
-              abstract
-              author
               banner {
                 childImageSharp {
                   fluid {
@@ -52,7 +85,6 @@ export const errorQuery = graphql`
                   }
                 }
               }
-              date(formatString: "MMMM DD, YYYY")
               path
               services
               tags
