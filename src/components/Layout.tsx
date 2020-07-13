962104e5b0a3ed91af6ccec6fe10430a30c6a42a
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-07-13 19:41:16 -0400

    added modal component to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import ThemeContext from '../context/ThemeContext';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Modal from './Modal';
 
 const Layout: React.FC = ({ children }) => (
 	<React.Fragment>
@@ -8,8 +10,11 @@ const Layout: React.FC = ({ children }) => (
 			Skip to main content
 		</a>
 		<Header />
-		<main className="main-content" id="mainContent">{children}</main>
+		<main className="Main" id="mainContent">
+			{children}
+		</main>
 		<Footer />
+		<ThemeContext.Consumer>{({ modalOpen }) => <Modal modalOpen={modalOpen} />}</ThemeContext.Consumer>
 	</React.Fragment>
 );
 
