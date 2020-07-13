3bcd56e0bb4046f9ca364aa02dde03b4a68c10f3
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-07-13 19:36:42 -0400

    removed portal package and component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,9 @@
+import React from 'react';
 import 'normalize.css';
 import './src/styles/global.scss';
+
+import { ThemeProvider } from './src/context/ThemeContext';
+
+export const wrapRootElement = ({ element }) => {
+	return <ThemeProvider>{element}</ThemeProvider>;
+};
