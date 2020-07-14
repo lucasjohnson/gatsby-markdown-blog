91e0489d47fc380fd1ff6932ddf7a9a38dcce16d
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-07-14 13:36:53 -0400

    added context logic to toggle hamburger

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const defaultState = {
 	contactOpen: false,
+	hamburgerOpen: false,
 	searchOpen: false,
 	toggleContact: () => {},
+	toggleHamburger: () => {},
 	toggleSearch: () => {}
 };
 
@@ -12,30 +14,39 @@ const ThemeContext = React.createContext(defaultState);
 class ThemeProvider extends React.Component {
 	state = {
 		contactOpen: false,
+		hamburgerOpen: false,
 		searchOpen: false
 	};
 
-	toggleSearch = () => {
-		this.setState((prevState) => ({ searchOpen: !prevState.searchOpen, contactOpen: false }));
+	toggleContact = () => {
+		this.setState((prevState) => ({ contactOpen: !prevState.contactOpen, hamburgerOpen: false, searchOpen: false }));
 	};
 
-	toggleContact = () => {
-		this.setState((prevState) => ({ contactOpen: !prevState.contactOpen, searchOpen: false }));
+	toggleHamburger = () => {
+		this.setState((prevState) => ({ contactOpen: false, hamburgerOpen: !prevState.hamburgerOpen, searchOpen: false }));
+	};
+
+	toggleSearch = () => {
+		this.setState((prevState) => ({ contactOpen: false, hamburgerOpen: false, searchOpen: !prevState.searchOpen }));
 	};
 
 	render() {
 		const { children } = this.props;
-		const { contactOpen, searchOpen } = this.state;
+		const { contactOpen, searchOpen, hamburgerOpen } = this.state;
 		const bodyElement: HTMLCollectionOf<HTMLBodyElement> = document.getElementsByTagName(`body`);
 
-		searchOpen || contactOpen ? (bodyElement[0].style.position = `fixed`) : (bodyElement[0].style.position = `static`);
+		searchOpen || contactOpen || hamburgerOpen
+			? (bodyElement[0].style.position = `fixed`)
+			: (bodyElement[0].style.position = `static`);
 
 		return (
 			<ThemeContext.Provider
 				value={{
 					contactOpen,
+					hamburgerOpen,
 					searchOpen,
 					toggleContact: this.toggleContact,
+					toggleHamburger: this.toggleHamburger,
 					toggleSearch: this.toggleSearch
 				}}
 			>
