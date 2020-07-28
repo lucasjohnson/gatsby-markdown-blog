f3613ca0a143dbfc06cf357ab43b4d9af917443f
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-07-28 14:34:53 -0400

    pagination component created

---
+++ src/components/Navigation/Pagination.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Pagination: React.FC<PaginationProps> = ({ posts }) => {
+	return <p>Pagination</p>;
+};
+
+interface PaginationProps {
+	posts: {}[];
+}
+
+export default Pagination;
